f7a93852ac586caed750acb48d106f76fa3a5a2a
portalchu <56888404+portalchu@users.noreply.github.com>
2022-10-25 17:40:15 +0900

    a

--- src/main/java/mock/SecretResolverInMemoryMock.java
+++ src/main/java/mock/SecretResolverInMemoryMock.java
@@ -1,13 +1,50 @@
 package mock;
 
+import org.didcommx.didcomm.common.VerificationMaterial;
+import org.didcommx.didcomm.common.VerificationMaterialFormat;
+import org.didcommx.didcomm.common.VerificationMethodType;
 import org.didcommx.didcomm.secret.Secret;
 import org.didcommx.didcomm.secret.SecretResolver;
+import org.didcommx.didcomm.secret.SecretResolverInMemory;
+import org.didcommx.didcomm.secret.SecretResolverEditable;
 
-public class SecretResolverInMemoryMock : SecretResolver {
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class SecretResolverInMemoryMock implements SecretResolverEditable {
+
+    private List<Secret> secrets;
+    public Secret secret1 = new Secret(
+            "did:example:alice#key-1",
+            VerificationMethodType.JSON_WEB_KEY_2020,
+            new VerificationMaterial(
+                    VerificationMaterialFormat.JWK,
+                    "{\"kty\":\"OKP\"," +
+                            "\"d\":\"f345a556a9b9cc7a48a28e21e2a91f728d91b69a3cbe8fadfd5383083c815923\"," +
+                            "\"crv\":\"Ed25519\",}"
+            )
+    );
+
 
     @Override
-    public List<Secret> getSecrets()
-        {
+    public Set<String> findKeys(List<String> list) {
+        return null;
+    }
 
-        }
+    @Override
+    public void addKey(Secret secret) {
+
+    }
+
+    @Override
+    public List<String> getKids() {
+        return null;
+    }
+
+    @Override
+    public Optional<Secret> findKey(String s) {
+        return Optional.empty();
+    }
 }
